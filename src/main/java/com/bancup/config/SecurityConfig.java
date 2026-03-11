465a57f1de844bc350ef3e6f2576f1cae6a896af
jesusfranck09 <d93409@gmail.com>
2026-03-11 00:39:01 -0600

    auth 41

--- src/main/java/com/bancup/config/SecurityConfig.java
+++ src/main/java/com/bancup/config/SecurityConfig.java
@@ -13,17 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-/**
- * Configuracion principal de Spring Security.
- *
- * Reglas:
- * - CSRF deshabilitado (API REST stateless)
- * - sesiones stateless
- * - CORS habilitado para frontend React
- * - acceso publico solo a signup, login y refresh
- * - cualquier otro endpoint requiere JWT valido
- * - JwtAuthenticationFilter se ejecuta antes del filtro de autenticacion estandar
- */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -34,16 +23,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
+
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
+
+                        // permitir preflight CORS
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                         .requestMatchers(HttpMethod.GET, "/auth/test-db").permitAll()
+
                         .requestMatchers(HttpMethod.POST, "/auth/signup", "/auth/login", "/auth/refresh").permitAll()
+
                         .anyRequest().authenticated()
                 )
+
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
